1adfe0776d89e598f3ff1bdfe33467d9a0f377a5
Jake Kaufman <jkaufman@lyft.com>
2018-08-26 16:13:49 -0400

    Give up on mod tap

--- keyboards/planck/keymaps/theevocater/keymap.c
+++ keyboards/planck/keymaps/theevocater/keymap.c
@@ -47,18 +47,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   q  |   w  |   e  |   r  |   t  |   y  |   u  |   i  |   o  |   p  | Bsls |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |CTLESC|   a  |   s  |   d  |   f  |   g  |   h  |   j  |   k  |   l  |   ;  |  "   |
+ * | CTRL |   a  |   s  |   d  |   f  |   g  |   h  |   j  |   k  |   l  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   z  |   x  |   c  |   v  |   b  |   n  |   m  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Alt  |  `   | Alt  | GUI  |Lower |Bspace| Space|Raise |      |   [  |   ]  |      |
+ * | Alt  |  `   | ESC  | GUI  |Lower |Bspace| Space|Raise |      |   [  |   ]  |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    CTRLESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,
-    KC_LALT, KC_GRV, KC_RALT, KC_LGUI, LOWER,   KC_BSPC,  KC_SPC,  RAISE,   _______, KC_LBRC, KC_RBRC,  _______
+    KC_LALT, KC_GRV, KC_ESC, KC_LGUI, LOWER,   KC_BSPC,  KC_SPC,  RAISE,   _______, KC_LBRC, KC_RBRC,  _______
 ),
 
 /* Lower
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   =  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | HOME |Pg Dn |Pg Up | END  |  \   |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | END  |Pg Dn |Pg Up | HOME |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-    _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_BSLS,
+    _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN, KC_PGUP, KC_HOME, _______, KC_BSLS,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
