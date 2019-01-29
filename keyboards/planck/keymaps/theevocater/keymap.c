64b0e4ffd86ff6a890ba5502dfb0eeb6f078de04
Jake Kaufman <jkaufman@lyft.com>
2019-01-28 22:31:13 -0500

    Final

--- keyboards/planck/keymaps/theevocater/keymap.c
+++ keyboards/planck/keymaps/theevocater/keymap.c
@@ -51,14 +51,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   z  |   x  |   c  |   v  |   b  |   n  |   m  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Alt  |  `   | ESC  | GUI  |Lower |Bspace| Space|Raise |      |   [  |   ]  |      |
+ * | Alt  |  `   | ESC  | GUI  |Lower |Bspace| Space|Raise |      |   [  |   ]  | ESC  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,
-    KC_LALT, KC_GRV, KC_ESC, KC_LGUI, LOWER,   KC_BSPC,  KC_SPC,  RAISE,   _______, KC_LBRC, KC_RBRC,  _______
+    KC_TAB,  KC_Q,   KC_W,    KC_E,   KC_R,  KC_T,    KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_LCTL, KC_A,   KC_S,    KC_D,   KC_F,  KC_G,    KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,   KC_X,    KC_C,   KC_V,  KC_B,    KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    KC_LALT, KC_GRV, KC_LGUI, KC_ESC, LOWER, KC_BSPC, KC_SPC, RAISE, _______, KC_LBRC, KC_RBRC, _______
 ),
 
 /* Lower
@@ -73,9 +73,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_PLUS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_UNDS,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD,  KC_VOLU, KC_MPLY
 ),
 
